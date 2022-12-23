dabdea05482c9741f67087351518f552a3766adb
Sandesh Thapa <sandeshthapa@Apples-Mac-mini.local>
2022-12-23 16:18:26 +0545

    added: trip-overview section without responsive

--- wp-content/themes/gh/trip-details.php
+++ wp-content/themes/gh/trip-details.php
@@ -93,39 +93,225 @@ the_post();
         <div class="container">
             <div class="tab-bar__row">
                 <ul class="tab-bar__list">
-                    <li><a href="#" class="active" id="trip__overview">Trip Overview</a></li>
-                    <li><a href="#" id="itinerary">Itinerary</a></li>
-                    <li><a href="#" id="includ__exclude">Include/Exclude</a></li>
-                    <li><a href="#" id="cost__dates">Cost & Dates</a></li>
-                    <li><a href="#" id="trip___info">Trip Info</a></li>
-                    <li><a href="#" id="Reviews">Reviews</a></li>
+                    <li><a href="#trip__overview" class="active">Trip Overview</a></li>
+                    <li><a href="#itinerary">Itinerary</a></li>
+                    <li><a href="#includ__exclude">Include/Exclude</a></li>
+                    <li><a href="#cost__dates">Cost & Dates</a></li>
+                    <li><a href="#trip___info">Trip Info</a></li>
+                    <li><a href="#Reviews">Reviews</a></li>
                 </ul>
             </div>
         </div>
     </section>
-    <section class="trip-overview">
+    <section class="trip-overview" id="trip__overview">
         <div class="container">
-            <div class="entry-content">
-                <h1>Highlights</h1>
-                <ul>
-                    <li>8% of all profits goes to support our <a href="#">philanthropy campaigns</a>.</li>
-                    <li>Hike to the Mt. Everest Base Camp (5,364m) and Kala Patthar (5,545m) view point.</li>
-                    <li>Lifetime adventurous journey towards the base camp of the tallest peak on earth.</li>
-                    <li>Superb views of the world’s highest peaks Mt. Everest (8,848.86m), Makalu (8,463m), Cho Oyu
-                        (8,201m), and Lhotse (8,516m).</li>
-                    <li>The impressive sight of the other mountains such as Amadablam (6,812m), Pumori (7,161m), and
-                        Nuptse (7,861 m).</li>
-                    <li>Discover Namche Bazar, which is the largest town in the heart of the Khumbu region.</li>
-                    <li>Explore the Sherpa settlements and their living style.</li>
-                    <li>Exciting mountain flight experiences.</li>
-                    <li>Visit the 17th-century oldest Tengboche monastery.</li>
-                    <li>Sightseeing tour at world heritage sites in the Kathmandu Valley.</li>
+            <div class="trip-overview__wrapper">
+                <div class="trip-overview__content entry-content">
+                    <h1 class="trip-overview__title">Highlights</h1>
+                    <ul>
+                        <li>8% of all profits goes to support our <a href="#">philanthropy campaigns</a>.</li>
+                        <li>Hike to the Mt. Everest Base Camp (5,364m) and Kala Patthar (5,545m) view point.</p>
+                        </li>
+                        <li>Lifetime adventurous journey towards the base camp of the tallest peak on earth.</li>
+                        <li>Superb views of the world’s highest peaks Mt. Everest (8,848.86m), Makalu (8,463m), Cho Oyu
+                            (8,201m), and Lhotse (8,516m).</li>
+                        <li>The impressive sight of the other mountains such as Amadablam (6,812m), Pumori (7,161m), and
+                            Nuptse (7,861 m).</li>
+                        <li>Discover Namche Bazar, which is the largest town in the heart of the Khumbu region.</li>
+                        <li>Explore the Sherpa settlements and their living style.</li>
+                        <li>Exciting mountain flight experiences.</li>
+                        <li>Visit the 17th-century oldest Tengboche monastery.</li>
+                        <li>Sightseeing tour at world heritage sites in the Kathmandu Valley.</li>
+                    </ul>
+                </div>
+                <div class="trip-overview__content entry-content">
+                    <div class="entry-content">
+                        <h1 class="trip-overview__title">Overview</h1>
+                        <p>5,364m Everest base camp trek goes through a legendary route where the landscapes & welcoming
+                            Sherpa’s touch your soul. So, hike between giants & test your mental strength as you view
+                            high
+                            mountain lakes & milky rivers. Likewise, you will also see lovely green valley’s loaded with
+                            purple blossoms if your trip to <a
+                                href="https://www.glorioushimalaya.com/about-nepal/">Nepal</a> is in spring. Life is
+                            about
+                            moments. Create them, don’t wait for them. There’s never a dull moment on the Everest base
+                            camp
+                            trek…</p>
+                        <p>Everest Base Camp <a href="https://www.glorioushimalaya.com/30-best-treks-in-nepal/">Trek in
+                                Nepal</a> is one of the hottest destinations for adventure anywhere on earth. In other
+                            words, it’s dreaming big when you book an Everest base camp trek. This is one of those trips
+                            that give meaning to your holiday dreams in the sunshine. For example, hiking on the
+                            legendary
+                            foothills just below the mighty mountains brings you closer to the world’s super peaks. That
+                            can
+                            only be the king of all peaks – <a
+                                href="https://www.glorioushimalaya.com/where-is-mount-everest-located/">Mt.Everest</a>.
+                            The
+                            breathtaking views of the master peaks are only in the <a
+                                href="https://www.glorioushimalaya.com/hiking/everest-region/">Everest Region</a>.</p>
+                    </div>
+                </div>
+                <div class="trip-overview__content entry-content">
+                    <div class="entry-content">
+                        <h1 class="trip-overview__title">Everest Base Camp Trek is all about self discovery…</h1>
+                        <p>We leave behind the comforts of our hotel in Kathmandu & take a fairytale flight to Lukla. At
+                            Lukla, we meet up with our friendly trekking team & hit the trails. We hike by the banks of
+                            the Dudh Kosi River through welcoming pretty Sherpa villages. After crossing several airy
+                            swing bridges, we walk into the bustling capital of <a
+                                href="https://www.glorioushimalaya.com/namche-bazaar/">Namche bazaar</a>.</p>
+
+                        <p>Our Everest base camp trek itinerary gives you enough time to acclimatize in this town.
+                            Namche sits within the Sagarmatha National Park, & is the capital of the Khumbu. Likewise,
+                            this is a natural spot to get familiar with the thin air at higher levels. Notwithstanding
+                            the great views in this area, we also visit the famous <a
+                                href="https://www.glorioushimalaya.com/tengboche-monastery/">Tengboche monastery</a>.
+                            This is the largest monastery in the region. There is so much we get to enjoy here. These
+                            mountains teach us a lot about life & ourselves.</p>
+                        <p>Famous climbers use the trails we now walk by to summit Everest. Some made it, some did not.
+                            However, these same trails offer us so much of unforgettable beauty. The trek is difficult
+                            but many cozy teahouses on these trails offer good food. The warm beds shelter us from
+                            unkind weather.</p>
+                        <p>We walk deep into the Khumbu, into forests of rhododendron, birch trees & pine. On the way,
+                            we come across colorful villages where we enjoy a sincere welcome by the <a
+                                href="https://www.glorioushimalaya.com/trekking-in-nepal-like-a-local-sherpa/">local
+                                Sherpa</a> people. We finally arrive at base camp, the main reason for our trip. The
+                            next day we climb up to Kalapatthar at 5,545m for a stunning early morning sunrise. The
+                            views from here are simply mind-blowing, leaving us breathless for a while!
+                            This is where our trip ends as we head back to Kathmandu via Namche to <a
+                                href="https://www.glorioushimalaya.com/lukla-airport/">Lukla</a> for our flight.</p>
+                    </div>
+                </div>
+                <div class="trip-overview__content entry-content">
+                    <div class="entry-content">
+                        <h1 class="trip-overview__title">Everest Base Camp Trek is all about self discovery…</h1>
+                        <p>Why shouldn’t you be so? You don’t need any technical experience for this great adventure.
+                            What you do need is some good walking habits of about 3-4 hours a day.</p>
+
+                        <p>Walking up the stairs to the 6th-7th floor of your building everyday would be ideal. You must
+                            be physically fit. This trek is fairly hard to difficult. But it can be done by anyone, from
+                            the young to the aged. This trek can even be done by greenhorns. You spend 11-12 days on the
+                            trail each day. Your max hiking time on the trails is 8 hours to a min of 3 hours uphill &
+                            downhill. While trekking you go higher by 400 to 800 meters each day. The highest point you
+                            must get to is 5,545m at <a href="https://www.glorioushimalaya.com/kala-patthar/">Kala
+                                Patthar</a> to view some of the best scenery on earth.</p>
+
+                        <p>The route is overall gentle on your body with some steep areas & some downhill hikes. You
+                            must hike through pretty rough gravel paths. There’s no need to worry about any technical
+                            moves, just go easy & hike to your pace. You will love it when you stop for your meals &
+                            some rest at the local mountain lodges. This is an adventure in the wild. Difficult roads
+                            often lead to beautiful destinations.</p>
+                    </div>
+                </div>
+                <div class="trip-overview__content entry-content">
+                    <div class="entry-content">
+                        <h1 class="trip-overview__title">Alternatives EBC Trek packages</h1>
+                        <p>We have other shorter versions of <a
+                                href="https://www.glorioushimalaya.com/trips/12-days-everest-base-camp-trek-itinerary/">12
+                                days EBC Trek</a> and <a
+                                href="https://www.glorioushimalaya.com/trips/14-days-everest-base-camp-trek-itinerary/">14
+                                days Trek to Everest Base Camp</a> packages. Moreover, would you like to trek in a
+                            luxurious way? Check out <a
+                                href="https://www.glorioushimalaya.com/trips/everest-base-camp-luxury-trek/">Everest
+                                Luxury Trek</a>.</p>
+                        <p>In the meantime, if you do not have enough time for this trek, you can opt for <a
+                                href="https://www.glorioushimalaya.com/other-activities/everest-helicopter-day-tour/">1
+                                day Everest helicopter tour</a>.</p>
+                    </div>
+                </div>
+                <div class="trip-overview__content entry-content">
+                    <div class="entry-content">
+                        <h1 class="trip-overview__title">Why Everest base camp trek with the GHT team?</h1>
+                        <p>Glorious Himalaya is an authentic tour company based in Kathmandu, Nepal. We are a team of
+                            expert mountain leaders having more than a decade of experience in adventure tourism. Below
+                            are some reasons to choose us.</p>
+                        <ol>
+                            <li>Over the 10 years, we are operating this venture for thousands of thrill seekers</li>
+                            <li>We give 8% of our profit to the Philanthropy campaign to support the needy locals.</li>
+                            <li>Owned by local experts.</li>
+                            <li>Deliver top-notch travel service to the customers.</li>
+                            <li>We operate tours by following the sustainable eco-tourism guidelines.</li>
+                            <li>Authentic local guides and porters having years of mountain experience.</li>
+                            <li>Provides hygienic foods and the best local lodges for your comfort stays in the mountain
+                                region.</li>
+                        </ol>
+                        <p><strong>Get other essential pieces of information about the Everest Base Camp Trek.</strong>
+                        </p>
+                        <ul>
+                            <li><a href="https://www.glorioushimalaya.com/what-to-pack-for-everest-base-camp-trek/">What
+                                    to pack for Everest Base Camp Trek?</a></li>
+                            <li><a href="https://www.glorioushimalaya.com/toilet-hot-shower-accommodations-on-everest/">Toilets,
+                                    Hot Shower and Accommodations on EBC Trek
+                                </a></li>
+                            <li><a
+                                    href="https://www.glorioushimalaya.com/wifi-internet-charging-facilities-on-everest-base-camp-trek/">Internet
+                                    & Charging Facilities on Everest Base Camp Trek</a></li>
+                            <li><a href="https://www.glorioushimalaya.com/best-time-for-everest-base-camp-trek/">Best
+                                    time for Everest Base Camp Trek.</a></li>
+                            <li><a href="https://www.glorioushimalaya.com/everest-base-camp-trek-training-program/">Everest
+                                    Base Camp Trek Training Program.</a></li>
+                            <li><a href="https://www.glorioushimalaya.com/everest-base-camp-trek-difficulty/">How
+                                    difficult is the EBC Trek?</a></li>
+                            <li><a href="https://www.glorioushimalaya.com/everest-base-camp-weather/">Everest Base Camp
+                                    Weather.</a></li>
+                            <li><a href="https://www.glorioushimalaya.com/everest-base-camp-trek-routes/">Everest Base
+                                    Camp Trek Routes.</a></li>
+                            <li><a href="https://www.glorioushimalaya.com/10-reasons-to-do-everest-base-camp-trek/">Top
+                                    10 Reasons to do Everest Base Camp Trek</a></li>
+                            <li><a href="https://www.glorioushimalaya.com/lukla-airport/">Lukla Airport</a></li>
+                            <li><a href="https://www.glorioushimalaya.com/kala-patthar/">Kala Patthar | Best Mount
+                                    Everest View Point</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="trip-overview__map">
+                <div class="trip-overview__map-wrapper">
+                    <h2 class="trip-overview__map-title">Route Map</h2>
+                    <figure class="trip-overview__media">
+                        <img src="<?php echo get_template_directory_uri(); ?>/uploads/map.jpg"
+                            alt="Image of a trekking" />
+                    </figure>
+                </div>
+                <a href="#">
+                    <div class="trip-overview__link">
+                        <p><strong>Any Questions?</strong><br>Please feel free to ask</p>
+                    </div>
+                </a>
+            </div>
+            <div class="trip-overview__mediaList">
+                <ul class="trip-overview__lists">
+                    <li><a href="#" target="_blank"><svg class="icon icon-icons8-twitter">
+                                <use xlink:href="#icon-icons8-twitter"></use>
+                            </svg></a></li>
+                    <li><a href="#" target="_blank"><svg class="icon icon-icons8-sms">
+                                <use xlink:href="#icon-icons8-sms"></use>
+                            </svg></a></li>
+                    <li><a href="#" target="_blank"><svg class="icon icon-icons8-whatsapp">
+                                <use xlink:href="#icon-icons8-whatsapp"></use>
+                            </svg></a></li>
+                    <li><a href="#" target="_blank"><svg class="icon icon-icons8-instagram_new">
+                                <use xlink:href="#icon-icons8-instagram_new"></use>
+                            </svg></a></li>
+                    <li><a href="#" target="_blank"><svg class="icon icon-icons8-facebook">
+                                <use xlink:href="#icon-icons8-facebook"></use>
+                            </svg></a></li>
+                    <li><a href="#" target="_blank"><svg class="icon icon-icons8-facebook_messenger">
+                                <use xlink:href="#icon-icons8-facebook_messenger"></use>
+                            </svg></a></li>
+
                 </ul>
             </div>
-            <div class="entry-content">
-               <p>5,364m Everest base camp trek goes through a legendary route where the landscapes & welcoming Sherpa’s touch your soul. So, hike between giants & test your mental strength as you view high mountain lakes & milky rivers. Likewise, you will also see lovely green valley’s loaded with purple blossoms if your trip to <a href="https://www.glorioushimalaya.com/about-nepal/">Nepal</a> is in spring. Life is about moments. Create them, don’t wait for them. There’s never a dull moment on the Everest base camp trek…</p>
-               <p>Everest Base Camp <a href="https://www.glorioushimalaya.com/30-best-treks-in-nepal/">Trek in Nepal</a> is one of the hottest destinations for adventure anywhere on earth. In other words, it’s dreaming big when you book an Everest base camp trek. This is one of those trips that give meaning to your holiday dreams in the sunshine. For example, hiking on the legendary foothills just below the mighty mountains brings you closer to the world’s super peaks. That can only be the king of all peaks – <a href="https://www.glorioushimalaya.com/where-is-mount-everest-located/">Mt.Everest</a>. The breathtaking views of the master peaks are only in the <a href="https://www.glorioushimalaya.com/hiking/everest-region/">Everest Region</a>.</p>
+
+        </div>
+    </section>
+    <section class="itinerary" id="itinerary">
+        <div class="container">
+            <div class="itinerary__link">
+                <a href="#">Expand All</a>
             </div>
+             <div class="itinerary__accordion">
+                <h2 class="itinerary__title"></h2>
+             </div>
         </div>
     </section>
     <?php include('modules/flex-content/flex-content.php') ?>
