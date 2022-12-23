b7dffa76d7bf52f6406ecb3badd50ade345093fd
Sandesh Thapa <sandeshthapa@Apples-Mac-mini.local>
2022-12-23 11:35:47 +0545

    added: breadcump and tab-bar section

--- wp-content/themes/gh/trip-details.php
+++ wp-content/themes/gh/trip-details.php
@@ -73,6 +73,61 @@ the_post();
             </div>
         </div>
     </section>
+    <section class="breadcump">
+        <div class="container">
+            <ul class="breadcump__list">
+                <li><a href="#" class="breadcump__links">Home</a><svg class="icon icon-breadcump">
+                        <use xlink:href="#icon-breadcump"></use>
+                    </svg> </li>
+                <li><a href="#" class="breadcump__links">Destination</a><svg class="icon icon-breadcump">
+                        <use xlink:href="#icon-breadcump"></use>
+                    </svg> </li>
+                <li><a href="#" class="breadcump__links">Trip</a><svg class="icon icon-breadcump">
+                        <use xlink:href="#icon-breadcump"></use>
+                    </svg> </li>
+                <li><a href="#" class="breadcump__links breadcump__links--alt">Everest Basecamp Trek</a></li>
+            </ul>
+        </div>
+    </section>
+    <section class="tab-bar">
+        <div class="container">
+            <div class="tab-bar__row">
+                <ul class="tab-bar__list">
+                    <li><a href="#" class="active" id="trip__overview">Trip Overview</a></li>
+                    <li><a href="#" id="itinerary">Itinerary</a></li>
+                    <li><a href="#" id="includ__exclude">Include/Exclude</a></li>
+                    <li><a href="#" id="cost__dates">Cost & Dates</a></li>
+                    <li><a href="#" id="trip___info">Trip Info</a></li>
+                    <li><a href="#" id="Reviews">Reviews</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class="trip-overview">
+        <div class="container">
+            <div class="entry-content">
+                <h1>Highlights</h1>
+                <ul>
+                    <li>8% of all profits goes to support our <a href="#">philanthropy campaigns</a>.</li>
+                    <li>Hike to the Mt. Everest Base Camp (5,364m) and Kala Patthar (5,545m) view point.</li>
+                    <li>Lifetime adventurous journey towards the base camp of the tallest peak on earth.</li>
+                    <li>Superb views of the world’s highest peaks Mt. Everest (8,848.86m), Makalu (8,463m), Cho Oyu
+                        (8,201m), and Lhotse (8,516m).</li>
+                    <li>The impressive sight of the other mountains such as Amadablam (6,812m), Pumori (7,161m), and
+                        Nuptse (7,861 m).</li>
+                    <li>Discover Namche Bazar, which is the largest town in the heart of the Khumbu region.</li>
+                    <li>Explore the Sherpa settlements and their living style.</li>
+                    <li>Exciting mountain flight experiences.</li>
+                    <li>Visit the 17th-century oldest Tengboche monastery.</li>
+                    <li>Sightseeing tour at world heritage sites in the Kathmandu Valley.</li>
+                </ul>
+            </div>
+            <div class="entry-content">
+               <p>5,364m Everest base camp trek goes through a legendary route where the landscapes & welcoming Sherpa’s touch your soul. So, hike between giants & test your mental strength as you view high mountain lakes & milky rivers. Likewise, you will also see lovely green valley’s loaded with purple blossoms if your trip to <a href="https://www.glorioushimalaya.com/about-nepal/">Nepal</a> is in spring. Life is about moments. Create them, don’t wait for them. There’s never a dull moment on the Everest base camp trek…</p>
+               <p>Everest Base Camp <a href="https://www.glorioushimalaya.com/30-best-treks-in-nepal/">Trek in Nepal</a> is one of the hottest destinations for adventure anywhere on earth. In other words, it’s dreaming big when you book an Everest base camp trek. This is one of those trips that give meaning to your holiday dreams in the sunshine. For example, hiking on the legendary foothills just below the mighty mountains brings you closer to the world’s super peaks. That can only be the king of all peaks – <a href="https://www.glorioushimalaya.com/where-is-mount-everest-located/">Mt.Everest</a>. The breathtaking views of the master peaks are only in the <a href="https://www.glorioushimalaya.com/hiking/everest-region/">Everest Region</a>.</p>
+            </div>
+        </div>
+    </section>
     <?php include('modules/flex-content/flex-content.php') ?>
 </main>
 <?php get_footer();
